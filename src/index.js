fc03b588b66730a3b4d369b0fc98923f9fb5cd0c
Andrei Roh <andrei_roh@tut.by>
2020-09-30 08:29:51 +0300

    init: start brackets task
    Added solution for brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  for (k = 0; k < bracketsConfig.length + 100; k++){
+    for (i = 0; i < bracketsConfig.length; i++){
+      for (j = 0; j <= str.length; j++){
+        if (str[j] + str[j + 1] == bracketsConfig[i].join('')){
+          str = str.replace(str[j] + str[j + 1],'');
+        }
+      }
+    }
+  }
+  if(str.length === 0){
+    return true
+    }
+  else {
+    return false
+    }
 }
